a47a18e169e0aa8c8b5cecb544997a4bda3bf31d
YOONHEEKWON <gmlrnjssla@naver.com>
2025-05-13 15:41:12 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import Board from "./board";
 function App() {
   return (
     <div>
-      {/* 기존 자기소개 영역 */}
-      <h1>안녕하세요!</h1>
-      {/* ...생략... */}
+      <h1>댓글 입력력</h1>
 
-      {/* 게시판 추가 */}
       <Board />
     </div>
   );
